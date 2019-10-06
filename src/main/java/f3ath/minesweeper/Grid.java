b218b46e4e8616a50be12477b02840cda825d47c
f3ath <karapetov@gmail.com>
2019-10-06 12:20:51 -0700

    Travis FTW

--- src/main/java/f3ath/minesweeper/Grid.java
+++ src/main/java/f3ath/minesweeper/Grid.java
@@ -15,47 +15,47 @@ public class Grid<T> {
         coordinates().forEach(c -> grid[c.getY()][c.getX()] = init.apply(c));
     }
 
-    public <U> Grid<U> map(Function<T, U> mapper) {
-        return map((t, c) -> mapper.apply(t));
+    public void forEachCell(Walker<Coordinate, T> walker) {
+        coordinates().forEach(c -> walker.apply(c, get(c)));
     }
 
-    public <U> Grid<U> map(Mapper<T, Coordinate, U> mapper) {
+    <U> Grid<U> map(Mapper<T, Coordinate, U> mapper) {
         return new Grid<>(width, height, c -> mapper.apply(get(c), c));
     }
 
-    public Grid<T> modify(Stream<Coordinate> coordinates, Mapper<T, Coordinate, T> mutator) {
+    Grid<T> modify(Stream<Coordinate> coordinates, Function<T, T> mutator) {
+        return modify(coordinates, (t, c) -> mutator.apply(t));
+    }
+
+
+    T get(Coordinate c) {
+        return grid[c.getY()][c.getX()];
+    }
+
+    int getWidth() {
+        return width;
+    }
+
+    int getHeight() {
+        return height;
+    }
+
+    boolean contains(Coordinate c) {
+        return c.getX() >= 0 && c.getX() < getWidth() && c.getY() >= 0 && c.getY() < getHeight();
+    }
+
+    Stream<T> cells() {
+        return coordinates().map(this::get);
+    }
+
+    private Grid<T> modify(Stream<Coordinate> coordinates, Mapper<T, Coordinate, T> mutator) {
         final var newGrid = map(Function.identity());
         coordinates.forEach(c -> newGrid.grid[c.getY()][c.getX()] = mutator.apply(newGrid.get(c), c));
         return newGrid;
     }
 
-    public Grid<T> modify(Stream<Coordinate> coordinates, Function<T, T> mutator) {
-        return modify(coordinates, (t, c) -> mutator.apply(t));
-    }
-
-    public void forEachCell(Walker<Coordinate, T> walker) {
-        coordinates().forEach(c -> walker.apply(c, get(c)));
-    }
-
-
-    public T get(Coordinate c) {
-        return grid[c.getY()][c.getX()];
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public boolean contains(Coordinate c) {
-        return c.getX() >= 0 && c.getX() < getWidth() && c.getY() >= 0 && c.getY() < getHeight();
-    }
-
-    public Stream<T> cells() {
-        return coordinates().map(this::get);
+    private <U> Grid<U> map(Function<T, U> mapper) {
+        return map((t, c) -> mapper.apply(t));
     }
 
     private Stream<Coordinate> coordinates() {
